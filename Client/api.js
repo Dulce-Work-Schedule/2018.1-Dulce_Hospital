92e1e3563f1b9b6f5f266995e01b593dfc7426fd
João Egewarth <egewarth@gmail.com>
2018-06-23 10:47:05 -0300

    WIP #21 Criando list by id
    Signed-off-by: João Egewarth <egewarth@gmail.com>

--- Client/api.js
+++ Client/api.js
@@ -1,19 +1,74 @@
+// The RegExp Object above validates MongoBD ObjectIds
+var checkObjectId = new RegExp('^[0-9a-fA-F]{24}$');
+
+function validate_field(field, result){
+  if (field.value == null || field.value == ''){
+    result[field.field_name + '_error'] = 'O campo ' + field.verbose + ' é obrigatório.';
+  } else if (typeof(field.value) != 'string') {
+    result[field.field_name + '_error'] = 'O campo ' + field.verbose +' deve ser uma string.';
+  }
+  return result;
+}
+
+function validate_id(field, result){
+  if (field.value == null || field.value == ''){
+    result[field.field_name + '_error'] = 'O campo ' + field.verbose + ' é obrigatório.';
+  } else if (!checkObjectId.test(field.value)) {
+    result[field.field_name + '_error'] = 'O ' + field.verbose +' é inválido.';
+  }
+  return result;
+}
+
 module.exports = function api(options){
-
   this.add('role:api,path:create', function(msg,respond){
+    var result = {};
+    var name = {
+     verbose: 'Nome do Hospital',
+     field_name: 'name'
+    }
+    name.value = msg.args.body.name;
 
-    var name = msg.args.body.name
-    var id = msg.args.query.id
+    result = validate_field(name, result);
 
-    this.act('role:hospital,cmd:create',{
-      name:name,
-      id:id
-    }, respond)
+    if (Object.entries(result)[0]) {
+      console.log("Result:");
+      console.log(result);
+      result.success = false;
+      respond(null, result)
+    // else, everything sucess
+    } else {
+      this.act('role:hospital,cmd:create',{
+        name:name.value
+      }, respond)
+    }
   })
 
-  this.add('role:api,path:listHospital', function(msg,respond) {
-    this.act('role:hospital, cmd:listHospital',{
-    },respond)
+  this.add('role:api,path:list', function(msg,respond) {
+    this.act('role:hospital, cmd:list',{},respond)
+  });
+
+  this.add('role:api,path:view', function(msg,respond) {
+    var hospital_id = msg.args.body.hospital_id
+    var result = {};
+    var hospital_id = {
+     verbose: 'Hospital',
+     field_name: 'hospital_id'
+    }
+    hospital_id.value = msg.args.query.hospital_id;
+
+    result = validate_field(hospital_id, result);
+
+    if (Object.entries(result)[0]) {
+      console.log("Result:");
+      console.log(result);
+      result.success = false;
+      respond(null, result)
+    // else, everything sucess
+    } else {
+      this.act('role:hospital, cmd:view',{
+        hospital_id: hospital_id
+      },respond)
+    }
   });
 
   this.add('role:api,path:error', function(msg, respond){
@@ -26,12 +81,9 @@ module.exports = function api(options){
       prefix: '/api/hospital',
       pin:    'role:api,path:*',
       map: {
-        create: { POST:true,
-                    auth: {
-                      strategy: 'jwt',
-                      fail: '/api/hospital/error',
-                    }},
-        listHospital: { GET:true,
+        create: { POST:true },
+        list: { GET:true },
+        view: { GET:true,
                     auth: {
                       strategy: 'jwt',
                       fail: '/api/hospital/error',
